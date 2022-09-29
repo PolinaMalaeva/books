56dcaecbcbef0dd430b81b6df28e075eddcf65bb
”PolinaMalaeva”~ <malaeva.polina@mail.ru>
2022-09-30 01:27:31 +0300

    added button back

--- Components/BookInfo.tsx
+++ Components/BookInfo.tsx
@@ -10,7 +10,15 @@ const BookInfo: FC<bookInfoProps> = ({book}) => {
     const {title, authors, download_count, formats, subjects} = book || {};
 
     return (
-        <div className="lg:flex sm:block py-19 gap-x-20 py-10">
+        <div className="lg:flex sm:block py-19 gap-x-20 py-10 relative">
+            <div className="absolute lg:left-4 top-[-70px] -left-4"
+                 onClick={() => window.location.pathname = '/books'}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                     stroke="currentColor" className="w-16 h-16 stroke-gray-600 hover:stroke-gray-800 cursor-pointer">
+                    <path
+                        d="M21 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953l7.108-4.062A1.125 1.125 0 0121 8.688v8.123zM11.25 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953L9.567 7.71a1.125 1.125 0 011.683.977v8.123z"/>
+                </svg>
+            </div>
             <div className="w-max mx-auto pb-8 basis-2/5 text-end">
                 <Image src={formats["image/jpeg"]}
                        alt="book"
